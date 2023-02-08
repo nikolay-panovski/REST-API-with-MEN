a008b8e19b218628f5b6edfb34f42f791cb3df46
Nikolay Panovski <nikipanovski@gmail.com>
2023-02-08 16:43:13 +0100

    Add models directory with product Schema

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+//const Schema = mongoose.Schema;   // cmd alias
+
+let productSchema = new mongoose.Schema(
+    
+    // product definition via properties
+
+    // tutorial product (tangible that one buys from an e-shop):
+
+    // { name: String
+    //   description: String, optional
+    //   price: Number
+    //   inStock: Boolean }
+
+    // a JSON Schema looks much like a JSON.
+    {
+        name: {type: String},                           // NB! Mongoose seems to not require type to be in " ".
+        description: {type: String, require: false},    // But default JSON Schemas seem to have {"type": "String" (or other) }.
+        price: {type: Number},
+        inStock: {type: Boolean}
+    }
+
+
+    // TODO: define my own "product" type to practice on scheming/creating for this assignment.
+    // (Twitch streams? or random videos? or just images?)
+    // TODO: write a Schema for it!
+)
+
+module.exports = mongoose.model("product", productSchema);
+/**
+ * https://mongoosejs.com/docs/models.html
+ * "The first argument is the singular name of the collection your model is for.
+ * Mongoose automatically looks for the plural, lowercased version of your model name.
+ * Thus, for the example above," the model 'product' SHOULD BE for the 'products' collection in the database,
+ * IF I understand correctly.
+ **/
