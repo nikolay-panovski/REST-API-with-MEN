f9e2546a8ee7e8d355b2c9b9778197ce44f4e846
Nikolay Panovski <nikipanovski@gmail.com>
2023-02-20 12:55:53 +0100

    Add GET routes with more advanced client params/responses

--- routes/product.js
+++ routes/product.js
@@ -40,10 +40,93 @@ router.post("/", (request, response) => {
         .catch(      error => { response.status(500).send( { message: error.message } ); } );
 });
 
+/**
 // Read all existing products - GET
     // SELF-EXERCISE: Try defining the steps in comments like above but alone, and then writing the code around them
     // (and from Mongoose documentation).
+router.get("/", (request, response) => {
+    // Request is to main URL - respond with all products.
+    // GET everything in database - find() query with no filter.
+    product.find()
+        // THEN: Compose a response for the client.
+        // Successful GET (200 OK) + send the data.
+        .then(obtainedData => { response.status(200).send(obtainedData); } )
+        // CATCH: Something failed. Generic failure (500 Internal Server Error) + raw caught error message.
+        .catch(error => { response.status(500).send( {message: error.message } ); } );
+
+        //.then(response.status(200).send(data))    // where data = product.find(), or at least that was the intention
+});
+/**/
+
+// !! Same as above but we modify the data before sending it back to the GET - see returnTailoredObject()
+router.get("/", (request, response) => {
+    product.find()
+        .then(obtainedData => { response.status(200).send(returnTailoredObject(obtainedData)); } )
+        .catch(error => { response.status(500).send( {message: error.message } ); } );
+});
+
+
+
+// Read all products IN STOCK - GET
+router.get("/instock", (request, response) => {
+    // HARDCODED URL
+
+    product.find( { inStock: true } )
+        .then(obtainedData => { response.status(200).send(obtainedData); } )
+        .catch(error => { response.status(500).send( { message: error.message } ); } );
+});
+
+router.get("/instock/:flag", (request, response) => {
+    const fl = request.params.flag;
+
+    product.find( { inStock: fl } )
+        .then(obtainedData => { response.status(200).send(obtainedData); } )
+        .catch(error => { response.status(500).send( { message: error.message } ); } );
+});
+
 // Read specific existing product - GET
+// :id here indicates TO EXPRESS to expect a parameter.
+// CORRESPONDS TO request.params.<key>, so: "/:myid" -> request.params.myid
+router.get("/:id", (request, response) => {
+    // Request is to specific id URL - filter response to that id.
+
+    // !! Does NOT correspond to Query > Query Parameters > anything, really. What are the works behind the magic of "/:id" ???
+
+    product.findById(request.params.id)     // equivalent (almost) to findOne({ _id: id }) for MongoDB
+        .then(obtainedData => { response.status(200).send(obtainedData); } )
+        .catch(error => { response.status(500).send( { message: error.message } ); } );
+});
+
+
+// Example in class: GET products/price/lt/1000 (lt = less than)
+router.get("/price/:operator/:price", (request, response) => {
+    const _operator = request.params.operator;
+    const _price = request.params.price;
+    
+    let filterExpression;
+
+    switch (_operator) {
+        case "lt":
+            filterExpression = { $lt: _price };
+            break;
+        case "lte":
+            filterExpression = { $lte: _price };
+            break;
+        case "gt":
+            filterExpression = { $gt: _price };
+            break;
+        case "gte":
+            filterExpression = { $gte: _price };
+            break;
+        default:
+            response.status(400).send( { message: "Bad price operator: " + _operator + ". Use lt, lte, gt, gte." } );
+            return;
+    }
+
+    product.find( { price: filterExpression } )
+        .then( (matchingData) => { response.status(200).send(returnTailoredObject(matchingData)) } )
+        .catch( (error) => response.status(500).send( { message: error.message } ) );
+});
 
 
 // Update specific existing product - PUT (notice: not PATCH here)
