d7562eff902efdc6a33f9bf235d1499084df085f
Nikolay Panovski <nikipanovski@gmail.com>
2023-02-27 14:49:48 +0100

    Include Swagger docs route in server API

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-// import dependencies
+// dependencies - main - Express (for NodeJS), Mongoose (for MongoDB)
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const app = express();
+
+// dependencies - Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+// OR: import YAML from "yamljs";
+const swaggerDefinition = YAML.load("./swagger.yaml");
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDefinition));
+
 
 // import routes (reference with or without .js does not matter)
 const productRoutes = require("./routes/product.js");
@@ -13,7 +21,7 @@ require("dotenv-flow").config();
 
 
 // parse request of Content-Type JSON
-app.use(bodyParser.json());
+app.use(express.json()); //app.use(bodyParser.json());
 
 
 mongoose.set("strictQuery", true);
