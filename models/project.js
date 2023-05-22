40fc6c8931d5b682a0af3626d6af0a9746d6593b
NikolayP <nikipanovski@gmail.com>
2023-05-22 19:01:16 +0200

    Remove faulty requirement for project's finished_at to be set

--- models/project.js
+++ models/project.js
@@ -7,7 +7,7 @@ let projectSchema = new mongoose.Schema(
         description: { type: String, required: false },
         company_client: { type: String, required: true /* type: "company" will NOT be created for this assignment */ },
         created_at: { type: Date, default: Date.now() },
-        finished_at: { type: Date, required: true, min: /*created_at*/Date.now() },
+        finished_at: { type: Date, min: /*created_at*/Date.now() },
         deadline: { type: Date, required: true },
         assignees: [ { type: mongoose.Schema.Types.ObjectId, ref: "user" } ],
         tasks: [ { type: mongoose.Schema.Types.ObjectId, ref: "task" } ],
